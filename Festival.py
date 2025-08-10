40a43419e2e4e66b4c4ec866241a33bd25f0be15
Laize Samilly <nobre.samilly@escolar.ifrn.edu.br>
2025-08-10 01:39:45 -0300

    correção de palco e main

--- Festival.py
+++ Festival.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 import uuid
-from datetime import date
+from datetime import date, timedelta
 
 # -------------------------------------------------
 # 1) Interface                                    🡇
@@ -78,9 +78,10 @@ class Cliente(Pessoa):
 
 class Funcionario (AuditavelMixin,IdentificavelMixin,Pessoa,Logavel):
     def __init__(self,cargo,nome,cpf):
-        super().__init__(nome,cpf)
+        Pessoa.__init__(self, nome, cpf)       
+        IdentificavelMixin.__init__(self)      
         self._cargo = cargo
-        self._registro=[]
+        self._registro = []
     def exibir_dados(self):
         return (f'nome:{self._nome}; cargo:{self._cargo}; registro:{self._registro}; ID:{self.get_id()}')
     def logar_entrada(self):
@@ -109,9 +110,9 @@ class Funcionario (AuditavelMixin,IdentificavelMixin,Pessoa,Logavel):
 
 
 class Festival:
-    def __init__(self,nome,data:date,local,capacidade,nome_palco,*valores):
+    def __init__(self,nome,local,capacidade,nome_palco,*valores):
         self.nome=nome
-        self.data=data
+        self.data= date.today()+timedelta(days=30)
         self.local=local
         self.palco= self.Palco(nome_palco,capacidade)
         self.clientes=[]
@@ -119,18 +120,21 @@ class Festival:
         self.ingressos=[*valores]
 
     def vender_ingresso(self,cliente: Cliente, ingresso: Ingresso):
+        ingresso_encontrado=False
         for i in self.ingressos:
-            if i.valor == ingresso.valor and i.codigo == ingresso.codigo and i.tipo == ingresso.tipo and self.palco.capacidade>=1:
+            if i.preco == ingresso.preco and i.codigo == ingresso.codigo and i.tipo == ingresso.tipo and (self.palco.capacidade - len(self.ingressos))>0:
                 if cliente in self.clientes:
                     pass
+
                 else:
                     self.clientes.append(cliente)
                 cliente.comprar_ingresso(i)
                 self.ingressos.remove(i)
-                self.palco.capacidade-=1
-                return
-            else:
-                raise ValueError ("INGRESSO NÃO DISPONÍVEL NO FESTIVAL")
+                ingresso_encontrado=True
+                return "Ingresso comprado!"
+            
+        if not ingresso_encontrado:
+            return "Ingresso não disponível no sistema do festival"            
 
         # if (self.palco.capacidade - len(self.clientes)) <=0:
         #     raise ValueError ("Ingressos esgotados!")
@@ -160,19 +164,19 @@ class Festival:
 
     def listar_ingressos(self):
         for i in self.ingressos:
-            print(" -", i)
+            print(i.__str__())
 
     
     class Palco:
         """Objeto que compõe o Festival."""
-    def __init__(self, nome: str, capacidade: int):
-        self.nome= nome
-        self.capacidade= capacidade
-        # TODO: armazenar nome e capacidade
+        def __init__(self, nome: str, capacidade: int):
+            self.nome= nome
+            self.capacidade= capacidade
+            # TODO: armazenar nome e capacidade
 
-    def resumo(self):
-        return (f'palco: {self.nome} - capacidade: {self.capacidade}')
-        # TODO: retornar string "Palco X – cap. Y pessoas"
+        def resumo(self):
+            return (f'palco: {self.nome} - capacidade: {self.capacidade}')
+            # TODO: retornar string "Palco X – cap. Y pessoas"
         
 
         
@@ -218,15 +222,17 @@ class EmpresaEventos:
         # TODO: validar + atualizar nome
     def adicionar_festival(self, festival: Festival):
         self.festivais.append(festival)
+        return f"festival {festival.nome} adicionado!"
         # TODO: adicionar festival à lista
         
     def buscar_festival(self, nome: str):
+        encontrado=False
         for i in self.festivais:
-            if i.nome == nome:
+            if i.nome == nome.nome:
                 encontrado=True
-                return "festival encontrado!"
+                return f"festival {nome.nome} encontrado!"
         if not encontrado:
-            return f"festival {nome} não encontrado!"
+            return f"festival {nome.nome} não encontrado!"
         # TODO: retornar festival ou None
 
     def listar_festivais(self):
@@ -239,13 +245,13 @@ class Auditor(IdentificavelMixin,Logavel):
         self.nome = nome
 
     def logar_entrada(self):
-        print(f"Nome: {self.nome} - Entrada: {date()}")
+        print(f"Nome: {self.nome} - Entrada: {date.today()}")
         
-    def auditar_festival(self,fest):
-        if fest.clientes == fest.palco.capacidade:
-            print( f'capacidade máxima atingida')
+    def auditar_festival(self,fest:Festival):
+        if len(fest.ingressos) >= fest.palco.capacidade:
+            print( f'capacidade máxima atingida!')
         else:
-            print(f'eu acho que cabe mais')
+            print(f'- VAGAS DISPONIVEIS: {fest.palco.capacidade - len(fest.ingressos)}')
 
         if len(fest.equipe) >=1:
             print(f'existe ao menos um funcionário')
@@ -274,8 +280,52 @@ class Auditor(IdentificavelMixin,Logavel):
 if __name__ == "__main__":
     """
     TODO:
-      • Crie 1 empresa, 2 festivais, clientes, equipe e auditor.
-      • Venda ingressos, liste participantes, audite festivais.
-      • Mostre saídas no console para validar implementações.
+      • Crie 1 empresa , 2 festivais, clientes, equipe e auditor. OK
+      • Venda ingressos, liste participantes, audite festivais. OK
+      • Mostre saídas no console para validar implementações. 
     """
-    pass
+    emp1=EmpresaEventos('Prefeitura de Padosferros')
+
+    ingresso_t1=Ingresso("1234","comum",25)
+    ingresso_t2=Ingresso("5678",'vip',250)
+    ingresso_t3=Ingresso("9011",'comum',25)
+    ingresso_t4=Ingresso("1213",'vip',250)
+    ingresso_t5=Ingresso("1415",'comum',25)
+
+    fest1=Festival("finecap","pau dos ferros",5000,"praça_eventos", ingresso_t1,ingresso_t2,ingresso_t3,ingresso_t4,ingresso_t5) #ingressos falta
+    fest2=Festival('jegue folia',"marcelino vieira",2000,"canto lá", ingresso_t3,ingresso_t5,ingresso_t1,ingresso_t2,ingresso_t4)
+
+    pissoa1=Cliente("Lucas","4002-8922","xaolin.matadordeporco@gmail.com")
+    pissoa2=Cliente("Somelnadona","666-999", "princesinhadoonedirection@gmail.com")
+    pissoa3=Cliente("Samael","50135-50738", "fonfon@gmail.com")
+    pissoa4=Cliente("Zé maria","666-666", "ohconrado@gmail.com")
+
+    funci1=Funcionario("garçom","gabriel","123.456.789-00")
+    funci2=Funcionario("bar man","mariano","123.456.789-11")
+    funci3=Funcionario("segurança","Xaden", "123.456.789-22")
+    funci4=Funcionario("faxineiro","creusa","123.456.789-33")
+
+    auditor_principal=Auditor('padrenosso')
+
+    funci1.logar_entrada()
+    funci2.logar_entrada()
+
+
+    print(fest1.vender_ingresso(pissoa1,ingresso_t1))
+    print(fest1.vender_ingresso(pissoa2,ingresso_t2))
+    print(fest1.vender_ingresso(pissoa3,ingresso_t3))
+
+    print(fest1.adicionar_funcionario(funci1))
+    print(fest1.adicionar_funcionario(funci2))
+
+    fest1.listar_clientes()
+    fest1.listar_ingressos()
+    fest1.listar_equipe()
+
+    auditor_principal.auditar_festival(fest1)
+    auditor_principal.logar_entrada()
+
+    print(emp1.adicionar_festival(fest1))
+    print(emp1.adicionar_festival(fest2))
+    print(emp1.buscar_festival(fest1))
+
